ad1dcb73ba5ecf76c2fb2310829a7c04dbd6e323
GeorgeOrzanescu <65309085+GeorgeOrzanescu@users.noreply.github.com>
2022-02-17 11:29:56 +0200

    Module 3

---
+++ 03_DynamicContent_TemplateEngines/app.js
@@ -0,0 +1,26 @@
+const path = require("path");
+
+const express = require("express");
+const bodyParser = require("body-parser");
+// const exphbs = require("express-handlebars");
+
+const app = express();
+
+// app.engine("handlebars", exphbs.engine({ defaultLayout: "main-layout" }));
+app.set("view engine", "ejs");
+
+const adminData = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/admin", adminData.routes);
+app.use(shopRoutes);
+
+app.use((req, res, next) => {
+  // res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
+  res.status(404).render("404", { pageTitle: "Page Not Found" });
+});
+
+app.listen(3000);
